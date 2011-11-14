890cf35b35aa9f57302990d88ab8c6cb9415f0b2
Vincent Jousse <vincent@jousse.org>
2011-11-14 21:30:06 +0100

    Bind xmonad keys for bépo

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,7 +1,9 @@
 import XMonad
 import XMonad.Layout.NoBorders
 import XMonad.Layout.Accordion
+
 import qualified Data.Map as M
+import qualified XMonad.StackSet as W
 
 
 main = do
@@ -23,9 +25,30 @@ newKeys x = M.union (keys defaultConfig x) (M.fromList (myKeys x))
 myKeys conf@(XConfig {XMonad.modMask = modMask}) =
     [
         -- Increase volume
-        ((0 , 0x1008ff11), spawn "amixer -q set Master 2- unmute")
+        ((0 , 0x1008ff11), spawn "amixer -q set PCM 2- unmute")
         -- Decrease Volume
-        , ((0 , 0x1008ff13), spawn "amixer -q set Master 2+ unmute")
+        , ((0 , 0x1008ff13), spawn "amixer -q set PCM 2+ unmute")
         -- Mute
-        , ((0 , 0x1008ff12), spawn "amixer -q set Master toggle")
+        , ((0 , 0x1008ff12), spawn "amixer -q set PCM toggle")
+
+        -- Move focus to the next window
+        , ((modMask,               xK_Tab   ), windows W.focusDown)
+    
+        -- Move focus to the next window
+        , ((modMask,               xK_t     ), windows W.focusDown)
+    
+        -- Move focus to the previous window
+        , ((modMask,               xK_s     ), windows W.focusUp  )
+    
+        -- Move focus to the master window
+        , ((modMask,               xK_m     ), windows W.focusMaster  )
+    
+        -- Swap the focused window and the master window
+        , ((modMask,               xK_Return), windows W.swapMaster)
+    
+        -- Swap the focused window with the next window
+        , ((modMask .|. shiftMask, xK_t     ), windows W.swapDown  )
+    
+        -- Swap the focused window with the previous window
+        , ((modMask .|. shiftMask, xK_s     ), windows W.swapUp    )
     ]
