3397d9415aa69c348491e7906a7972cbd09ea155
Vincent Jousse <vincent@jousse.org>
2011-11-19 11:33:24 +0100

    Fix git prompt

--- zsh/git.zsh
+++ zsh/git.zsh
@@ -1,11 +1,6 @@
 # get the name of the branch we are on
 function git_prompt_info_custom() {
-    [ ! -f .git/HEAD ] && return
-    ref=$(git symbolic-ref HEAD 2> /dev/null) || ref="*detached*"
-    prompt="$ZSH_THEME_GIT_PROMPT_PREFIX"
-    prompt="$prompt${ref#refs/heads/}"
-    prompt="$prompt$(git_prompt_short_sha)"
-    prompt="$prompt$ZSH_THEME_GIT_PROMPT_DIRTY"
-    prompt="$prompt$ZSH_THEME_GIT_PROMPT_SUFFIX"
-    echo $prompt
+  ref=$(git symbolic-ref HEAD 2> /dev/null) || return
+  echo "$ZSH_THEME_GIT_PROMPT_PREFIX${ref#refs/heads/}$(git_prompt_short_sha)$(parse_git_dirty)$ZSH_THEME_GIT_PROMPT_SUFFIX"
 }
+
