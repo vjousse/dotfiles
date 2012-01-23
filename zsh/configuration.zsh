c0569c479288775573e86bbcc9d0056b99a5e791
Vincent Jousse <vincent@jousse.org>
2012-01-23 09:15:31 +0100

    Add sbt/java options

--- zsh/configuration.zsh
+++ zsh/configuration.zsh
@@ -1,5 +1,10 @@
 # Export common variables
-export PATH="$PATH:$HOME/usr/bin:$DOTFILES/scripts:$DOTFILES/scripts/private:$HOME/usr/src/scala/play-2.0"
+export PATH="$PATH:$HOME/usr/bin/ant-tools:$HOME/usr/bin:$DOTFILES/scripts:$DOTFILES/scripts/private:$HOME/usr/src/scala/play-2.0"
+
+# Java/SBT configuration
+export SBT_OPTS="-Dfile.encoding=UTF8 -Xms256M -Xmx2048M -Xss4M -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=2048M"
+export _JAVA_AWT_WM_NONREPARENTING=1
+
 export BROWSER="firefox"
 export EDITOR='vim'
 export GIT_EDITOR='vim'
