78d5420e80f32d77cb79e4dd7e7aded89a0aab80
Vincent Jousse <vincent@jousse.org>
2012-01-31 15:04:13 +0100

    Add useful function to set git upstream branch

--- zsh/git.zsh
+++ zsh/git.zsh
@@ -4,3 +4,9 @@ function git_prompt_info_custom() {
   echo "$ZSH_THEME_GIT_PROMPT_PREFIX${ref#refs/heads/}$(git_prompt_short_sha)$(parse_git_dirty)$ZSH_THEME_GIT_PROMPT_SUFFIX"
 }
 
+# Set the upstream branch
+git-set-upstream() {
+    [ -z $1 ] && 1=origin
+    branch=$(git-current-branch)
+    git branch --set-upstream $branch $1/$branch
+}
