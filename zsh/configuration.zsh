ce379dd1c083d211e7a37283ad0b031112c1071a
Vincent Jousse <vincent@jousse.org>
2012-05-20 21:51:54 +0200

    Use Java 1.6

--- zsh/configuration.zsh
+++ zsh/configuration.zsh
@@ -1,5 +1,6 @@
 # Export common variables
-export PATH="$PATH:$HOME/usr/bin/ant-tools:$HOME/usr/bin:$DOTFILES/scripts:$DOTFILES/scripts/private:$HOME/usr/src/scala/play-2.0"
+export JAVA_HOME=$HOME/usr/bin/jdk1.6.0_30
+export PATH="$JAVA_HOME/bin:$PATH:$HOME/usr/bin/ant-tools:$HOME/usr/bin:$DOTFILES/scripts:$DOTFILES/scripts/private:$HOME/usr/src/scala/play-2.0:/usr/share/java/apache-ant/bin"
 
 # Java/SBT configuration
 export SBT_OPTS="-Dfile.encoding=UTF8 -Xms256M -Xmx2048M -Xss4M -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=2048M"
