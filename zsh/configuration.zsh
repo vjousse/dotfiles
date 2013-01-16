cc2f3cb8ccdf39757f5116bca51cbb47c8061382
Vincent Jousse <vincent@jousse.org>
2013-01-16 15:09:17 +0100

    Use play 2.1

--- zsh/configuration.zsh
+++ zsh/configuration.zsh
@@ -1,6 +1,6 @@
 # Export common variables
 #export JAVA_HOME=$HOME/usr/bin/jdk1.6.0_30
-export PATH="$JAVA_HOME/bin:$PATH:$HOME/usr/bin/ant-tools:$HOME/usr/bin:$DOTFILES/scripts:$DOTFILES/scripts/private:$HOME/usr/src/scala/play-2.0.4:/usr/share/java/apache-ant/bin:/home/vjousse/.gem/ruby/1.9.1/bin"
+export PATH="$JAVA_HOME/bin:$PATH:$HOME/usr/bin/ant-tools:$HOME/usr/bin:$DOTFILES/scripts:$DOTFILES/scripts/private:$HOME/usr/src/scala/play-2.1-RC1:/usr/share/java/apache-ant/bin:/home/vjousse/.gem/ruby/1.9.1/bin"
 
 # Java/SBT configuration
 export SBT_OPTS="-Dfile.encoding=UTF8 -Xms256M -Xmx2048M -Xss4M -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=2048M"
