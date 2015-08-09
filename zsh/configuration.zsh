2fad342a348b8407ff2bd1c233a8d0e919a18267
Vincent Jousse <vincent@jousse.org>
2015-08-09 22:18:37 +0200

    Remove deprecated maxpermsize for java 8

--- zsh/configuration.zsh
+++ zsh/configuration.zsh
@@ -3,7 +3,7 @@
 export PATH="$JAVA_HOME/bin:$PATH:$HOME/usr/bin/ant-tools:$HOME/usr/bin:$DOTFILES/scripts:$DOTFILES/scripts/private:$HOME/usr/src/scala/play-2.2.1:/usr/share/java/apache-ant/bin:/home/vjousse/.gem/ruby/1.9.1/bin:/home/vjousse/.cabal/bin"
 
 # Java/SBT configuration
-export SBT_OPTS="-Dfile.encoding=UTF8 -Xms256M -Xmx2048M -Xss4M -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=2048M"
+export SBT_OPTS="-Dfile.encoding=UTF8 -Xms256M -Xmx2048M -Xss4M -XX:+CMSClassUnloadingEnabled"
 export _JAVA_AWT_WM_NONREPARENTING=1
 
 export BROWSER="firefox"
