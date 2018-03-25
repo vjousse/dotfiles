24748cc1c74b55abaf5e57c343c8f7b98da10bb0
Vincent Jousse <vincent@jousse.org>
2018-03-25 10:20:07 +0200

    Add gopath

--- zsh/configuration.zsh
+++ zsh/configuration.zsh
@@ -1,6 +1,9 @@
 # Export common variables
 #export JAVA_HOME=$HOME/usr/bin/jdk1.6.0_30
-export PATH="$JAVA_HOME/bin:$PATH:$HOME/usr/bin/ant-tools:$HOME/usr/bin:$DOTFILES/scripts:$DOTFILES/scripts/private:$HOME/usr/src/scala/play-2.2.1:/usr/share/java/apache-ant/bin:/home/vjousse/.cabal/bin:$HOME/.gem/ruby/2.5.0/bin:$HOME/.npm-global/bin:$HOME/.local/bin/"
+export GOPATH=$HOME/usr/src/go
+export GOBIN=$GOPATH/bin
+
+export PATH="./node_modules/elm-format/bin/:$JAVA_HOME/bin:$PATH:$HOME/usr/bin/ant-tools:$HOME/usr/bin:$DOTFILES/scripts:$DOTFILES/scripts/private:$HOME/usr/src/scala/play-2.2.1:/usr/share/java/apache-ant/bin:/home/vjousse/.cabal/bin:$HOME/.gem/ruby/2.5.0/bin:$HOME/.npm-global/bin:$HOME/.local/bin/:$GOROOT/bin:$GOBIN"
 
 # Java/SBT configuration
 export SBT_OPTS="-Dfile.encoding=UTF8 -Xms256M -Xmx2048M -Xss4M -XX:+CMSClassUnloadingEnabled"
@@ -10,3 +13,11 @@ export BROWSER="firefox-nightly"
 export EDITOR='nvim'
 export GIT_EDITOR='nvim'
 export CLASSPATH=$CLASSPATH:$HOME/usr/lib
+
+NPM_PACKAGES="${HOME}/.npm-packages"
+
+export PATH="$NPM_PACKAGES/bin:$PATH"
+
+# Unset manpath so we can inherit from /etc/manpath via the `manpath` command
+unset MANPATH # delete if you already modified MANPATH elsewhere in your config
+export MANPATH="$NPM_PACKAGES/share/man:$(manpath)"
