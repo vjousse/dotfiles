3f5fae35d2bb2706c367cf1cbe912d434d51b70f
Vincent Jousse <vincent@jousse.org>
2022-05-28 20:09:55 +0200

    Clean Zsh mess

--- zsh/configuration.zsh
+++ zsh/configuration.zsh
@@ -1,34 +1,22 @@
 # Export common variables
-#export JAVA_HOME=$HOME/usr/bin/jdk1.6.0_30
-export GOPATH=$HOME/usr/src/go
-export GOBIN=$GOPATH/bin
 export CARGOBIN=$HOME/.cargo/bin
+export VOLTA_HOME="$HOME/.volta"
+NPM_PACKAGES="${HOME}/.npm-packages"
 
-export PATH="./node_modules/elm-format/bin/:$JAVA_HOME/bin:$PATH:$HOME/usr/bin/ant-tools:$HOME/usr/bin:$DOTFILES/scripts:$DOTFILES/scripts/private:$HOME/usr/src/scala/play-2.2.1:/usr/share/java/apache-ant/bin:/home/vjousse/.cabal/bin:$HOME/.gem/ruby/2.7.0/bin:$HOME/.npm-global/bin:$HOME/.local/bin/:$GOROOT/bin:$GOBIN:$CARGOBIN:$HOME/.yarn/bin:$HOME/.local/bin:$HOME/.gem/ruby/3.0.0/bin"
+export PATH="$VOLTA_HOME/bin:$HOME/usr/bin:$DOTFILES/scripts:$HOME/.local/bin/:$CARGOBIN:$HOME/.yarn/bin:$NPM_PACKAGES/bin:$PATH"
 
 export PYTHONPATH=".:$PYTHONPATH"
-
-export ANDROID_SDK=/opt/android-sdk
-
-export TODO_DIR="$HOME/Nextcloud"
-export TODO_FILE="$TODO_DIR/todo.txt"
-export DONE_FILE="$TODO_DIR/done.txt"
-
-# Java/SBT configuration
-export SBT_OPTS="-Dfile.encoding=UTF8 -Xms256M -Xmx2048M -Xss4M -XX:+CMSClassUnloadingEnabled"
-export _JAVA_AWT_WM_NONREPARENTING=1
 
 export BROWSER="firefox-nightly"
 export EDITOR='nvim'
 export GIT_EDITOR='nvim'
 
-NPM_PACKAGES="${HOME}/.npm-packages"
-
-export PATH="$NPM_PACKAGES/bin:$PATH"
-
 # Unset manpath so we can inherit from /etc/manpath via the `manpath` command
 unset MANPATH # delete if you already modified MANPATH elsewhere in your config
 export MANPATH="$NPM_PACKAGES/share/man:$(manpath)"
 
+export PYENV_ROOT="$HOME/.pyenv"
+command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init -)"
 
-. $HOME/.asdf/asdf.sh
+eval "$(pyenv virtualenv-init -)"
