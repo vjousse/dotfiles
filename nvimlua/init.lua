485ae265ae9bdf069e1470eaf1028f6df783f639
Vincent Jousse <vincent@jousse.org>
2023-01-12 19:35:58 +0100

    Add nvimtree config to remove keybinding conflict

--- nvimlua/init.lua
+++ nvimlua/init.lua
@@ -2,17 +2,16 @@
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
-require "config.options"
-require "config.keymaps"
-require "config.plugins"
-require "config.colorscheme"
-require "config.cmp"
-require "config.indentline"
-require "config.lsp"
-require "config.telescope"
-require "config.treesitter"
-require "config.autopairs"
-require "config.bufferline"
-require "config.gitsigns"
-
-require("nvim-tree").setup()
+require("config.options")
+require("config.keymaps")
+require("config.plugins")
+require("config.colorscheme")
+require("config.cmp")
+require("config.indentline")
+require("config.lsp")
+require("config.telescope")
+require("config.treesitter")
+require("config.autopairs")
+require("config.bufferline")
+require("config.gitsigns")
+require("config.nvimtree")
