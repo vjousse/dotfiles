f9ee57a67019b635fa335af75b5e60cbec6d0b9b
Vincent Jousse <vincent@jousse.org>
2023-04-06 15:57:45 +0200

    Display filepath in lualine

--- nvimlua/lua/config/lualine.lua
+++ nvimlua/lua/config/lualine.lua
@@ -25,7 +25,7 @@ lualine.setup({
 	sections = {
 		lualine_a = { "mode" },
 		lualine_b = { "branch", "diff", "diagnostics" },
-		lualine_c = { "filename" },
+		lualine_c = { { "filename", path = 1 } },
 		lualine_x = { "encoding", "fileformat", "filetype" },
 		lualine_y = { "progress" },
 		lualine_z = { "location" },
