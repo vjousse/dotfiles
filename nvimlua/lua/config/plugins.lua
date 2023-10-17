e17f869099827d00701e316397aff877e4527fca
Vincent Jousse <vincent@jousse.org>
2023-10-17 14:12:56 +0200

    Update telescope

--- nvimlua/lua/config/plugins.lua
+++ nvimlua/lua/config/plugins.lua
@@ -94,8 +94,7 @@ return packer.startup(function(use)
 
 	use({
 		"nvim-telescope/telescope.nvim",
-		tag = "0.1.0",
-		-- or                            , branch = '0.1.x',
+		branch = "0.1.x",
 		requires = { { "nvim-lua/plenary.nvim" } },
 	})
 
