b31cc107901019a8dc207e7fa82c4fdf11b71580
Vincent Jousse <vincent@jousse.org>
2023-10-17 11:07:17 +0200

    Migrate indent blank line to v3

--- nvimlua/lua/config/indentline.lua
+++ nvimlua/lua/config/indentline.lua
@@ -1,18 +1,6 @@
-local status_ok, indent_blankline = pcall(require, "indent_blankline")
+local status_ok, indent_blankline = pcall(require, "ibl")
 if not status_ok then
-  return
+	return
 end
 
-indent_blankline.setup {
-  char = "▏",
-  show_trailing_blankline_indent = false,
-  show_first_indent_level = true,
-  use_treesitter = true,
-  show_current_context = true,
-  buftype_exclude = { "terminal", "nofile" },
-  filetype_exclude = {
-    "help",
-    "packer",
-    "NvimTree",
-  },
-}
+indent_blankline.setup()
