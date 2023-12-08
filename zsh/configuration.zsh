d35564c9e7fd3da7c2a51eef214cb3d5bac0d48e
Vincent Jousse <vincent@jousse.org>
2023-12-08 17:15:39 +0100

    Use Sway/Wayland instead of i3/Xorg

--- zsh/configuration.zsh
+++ zsh/configuration.zsh
@@ -12,6 +12,7 @@ export PYTHONPATH=".:$PYTHONPATH"
 export BROWSER="firefox-nightly"
 export EDITOR='nvim'
 export GIT_EDITOR='nvim'
+export GTK_USE_PORTAL=0
 
 # Unset manpath so we can inherit from /etc/manpath via the `manpath` command
 unset MANPATH # delete if you already modified MANPATH elsewhere in your config
