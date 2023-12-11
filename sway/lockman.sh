1b6e5ae7108be6db529ee6c14402b4b3c564e2b4
Vincent Jousse <vincent@jousse.org>
2023-12-11 14:25:22 +0100

    Add screenlock
    Using swaylock effects https://github.com/mortie/swaylock-effects

---
+++ sway/lockman.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# Times the screen off and puts it to background
+swayidle \
+    timeout 10 'swaymsg "output * dpms off"' \
+    resume 'swaymsg "output * dpms on"' &
+# Locks the screen immediately
+swaylock --clock --indicator --screenshots --effect-scale 0.4 --effect-vignette 0.2:0.5 --effect-blur 4x2 --datestr "%a %e.%m.%Y" --timestr "%k:%M"
+# Kills last background task so idle timer doesn't keep running
+kill %%
