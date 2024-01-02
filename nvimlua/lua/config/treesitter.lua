04e4b8ae429f4342f15bebac65d3df81c6f188c9
Vincent Jousse <vincent@jousse.org>
2024-01-02 09:57:32 +0100

    Fix deprecated commentstring config

--- nvimlua/lua/config/treesitter.lua
+++ nvimlua/lua/config/treesitter.lua
@@ -9,29 +9,51 @@ if not status_ok then
 end
 
 configs.setup({
-  ensure_installed = { "lua", "markdown", "markdown_inline", "bash", "python", "graphql", "javascript", "rasi", "rust", "typescript", "vim", "yaml", "svelte", "rst", "html", "json", "gitignore", "dockerfile" }, -- put the language you want in this array
-  -- ensure_installed = "all", -- one of "all" or a list of languages
+	ensure_installed = {
+		"lua",
+		"markdown",
+		"markdown_inline",
+		"bash",
+		"python",
+		"graphql",
+		"javascript",
+		"rasi",
+		"rust",
+		"typescript",
+		"vim",
+		"yaml",
+		"svelte",
+		"rst",
+		"html",
+		"json",
+		"gitignore",
+		"dockerfile",
+	}, -- put the language you want in this array
+	-- ensure_installed = "all", -- one of "all" or a list of languages
 	ignore_install = { "" }, -- List of parsers to ignore installing
 	sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
-  highlight = {
+	highlight = {
 		enable = true, -- false will disable the whole extension
-		disable = { }, -- list of language that will be disabled
+		disable = {}, -- list of language that will be disabled
 	},
 	autopairs = {
 		enable = true,
 	},
-	indent = { enable = true, disable = { } },
+	indent = { enable = true, disable = {} },
 
-	context_commentstring = {
+	rainbow = {
 		enable = true,
-		enable_autocmd = false,
+		-- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
+		extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
+		max_file_lines = nil, -- Do not enable for files with more than n lines, int
+		-- colors = {}, -- table of hex strings
+		-- termcolors = {} -- table of colour name strings
 	},
-  rainbow = {
-    enable = true,
-    -- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
-    extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
-    max_file_lines = nil, -- Do not enable for files with more than n lines, int
-    -- colors = {}, -- table of hex strings
-    -- termcolors = {} -- table of colour name strings
-  }
+})
+
+require("ts_context_commentstring").setup({
+	enable_autocmd = false,
+	languages = {
+		typescript = "// %s",
+	},
 })
