c5582322eb5486f37e6d0444a25e7a593757e2a9
Vincent Jousse <vincent@jousse.org>
2024-05-29 17:36:57 +0200

    Enable line wrapping

--- nvim-lazy/lua/core/options.lua
+++ nvim-lazy/lua/core/options.lua
@@ -10,9 +10,6 @@ opt.shiftwidth = 2 -- 2 spaces for indent width
 opt.expandtab = true -- expand tab to spaces
 opt.autoindent = true -- copy indent from current line when starting new one
 
--- line wrapping
-opt.wrap = false -- disable line wrapping
-
 -- search settings
 opt.ignorecase = true -- ignore case when searching
 opt.smartcase = true -- if you include mixed case in your search, assumes you want case-sensitive
