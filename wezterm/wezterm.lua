f3627115806170b053e6e1826d21832d81895776
Vincent Jousse <vincent@jousse.org>
2024-05-30 13:49:58 +0200

    Tweak shortcuts for copy mode

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,11 +1,22 @@
 -- Pull in the wezterm API
 local wezterm = require("wezterm")
+local act = wezterm.action
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices
 --
+
+local copy_mode = nil
+if wezterm.gui then
+	copy_mode = wezterm.gui.default_key_tables().copy_mode
+	table.insert(copy_mode, { key = "c", mods = "NONE", action = act.CopyMode("MoveLeft") })
+	table.insert(copy_mode, { key = "t", mods = "NONE", action = act.CopyMode("MoveDown") })
+	table.insert(copy_mode, { key = "s", mods = "NONE", action = act.CopyMode("MoveUp") })
+	table.insert(copy_mode, { key = "r", mods = "NONE", action = act.CopyMode("MoveRight") })
+	table.insert(copy_mode, { key = "é", mods = "NONE", action = act.CopyMode("MoveForwardWord") })
+end
 
 -- For example, changing the color scheme:
 config.color_scheme = "Tokyo Night"
@@ -23,6 +34,15 @@ config.keys = {
 	-- paste from the primary selection
 	{ key = "V", mods = "CTRL|SHIFT", action = wezterm.action.PasteFrom("PrimarySelection") },
 
+	{ key = "b", mods = "CTRL", action = wezterm.action.ScrollByPage(-1) },
+	{ key = "f", mods = "CTRL", action = wezterm.action.ScrollByPage(1) },
+
+	{ key = "s", mods = "CTRL|SHIFT", action = act.ScrollToPrompt(-1) },
+	{ key = "t", mods = "CTRL|SHIFT", action = act.ScrollToPrompt(1) },
+
+	{ key = "l", mods = "ALT|SHIFT", action = act.ActivateCopyMode },
+
+	-- display hints to open links with a keyboard key
 	{
 		key = "L",
 		mods = "SHIFT|CTRL",
@@ -38,6 +58,10 @@ config.keys = {
 			end),
 		}),
 	},
+}
+
+config.key_tables = {
+	copy_mode = copy_mode,
 }
 
 -- and finally, return the configuration to wezterm
