601bf2efe0bbfbc9ed7742b6ed1c489d7e281dac
Vincent Jousse <vincent@jousse.org>
2024-07-11 20:19:14 +0200

    fix: change wezterm borders

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -10,12 +10,12 @@ local config = wezterm.config_builder()
 
 local copy_mode = nil
 if wezterm.gui then
-	copy_mode = wezterm.gui.default_key_tables().copy_mode
-	table.insert(copy_mode, { key = "c", mods = "NONE", action = act.CopyMode("MoveLeft") })
-	table.insert(copy_mode, { key = "t", mods = "NONE", action = act.CopyMode("MoveDown") })
-	table.insert(copy_mode, { key = "s", mods = "NONE", action = act.CopyMode("MoveUp") })
-	table.insert(copy_mode, { key = "r", mods = "NONE", action = act.CopyMode("MoveRight") })
-	table.insert(copy_mode, { key = "é", mods = "NONE", action = act.CopyMode("MoveForwardWord") })
+  copy_mode = wezterm.gui.default_key_tables().copy_mode
+  table.insert(copy_mode, { key = "c", mods = "NONE", action = act.CopyMode("MoveLeft") })
+  table.insert(copy_mode, { key = "t", mods = "NONE", action = act.CopyMode("MoveDown") })
+  table.insert(copy_mode, { key = "s", mods = "NONE", action = act.CopyMode("MoveUp") })
+  table.insert(copy_mode, { key = "r", mods = "NONE", action = act.CopyMode("MoveRight") })
+  table.insert(copy_mode, { key = "é", mods = "NONE", action = act.CopyMode("MoveForwardWord") })
 end
 
 -- For example, changing the color scheme:
@@ -26,42 +26,49 @@ config.font = wezterm.font("FiraCode Nerd Font")
 config.hide_tab_bar_if_only_one_tab = true
 
 config.keys = {
-	{ key = "Enter", mods = "ALT|SHIFT", action = wezterm.action.SpawnWindow },
+  { key = "Enter", mods = "ALT|SHIFT", action = wezterm.action.SpawnWindow },
 
-	-- paste from clipboard
-	{ key = "V", mods = "ALT|SHIFT", action = wezterm.action.PasteFrom("Clipboard") },
+  -- paste from clipboard
+  { key = "V", mods = "ALT|SHIFT", action = wezterm.action.PasteFrom("Clipboard") },
 
-	-- paste from the primary selection
-	{ key = "V", mods = "CTRL|SHIFT", action = wezterm.action.PasteFrom("PrimarySelection") },
+  -- paste from the primary selection
+  { key = "V", mods = "CTRL|SHIFT", action = wezterm.action.PasteFrom("PrimarySelection") },
 
-	{ key = "b", mods = "ALT|SHIFT", action = wezterm.action.ScrollByPage(-1) },
-	{ key = "p", mods = "ALT|SHIFT", action = wezterm.action.ScrollByPage(1) },
+  { key = "b", mods = "ALT|SHIFT", action = wezterm.action.ScrollByPage(-1) },
+  { key = "p", mods = "ALT|SHIFT", action = wezterm.action.ScrollByPage(1) },
 
-	{ key = "s", mods = "CTRL|SHIFT", action = act.ScrollToPrompt(-1) },
-	{ key = "t", mods = "CTRL|SHIFT", action = act.ScrollToPrompt(1) },
+  { key = "s", mods = "CTRL|SHIFT", action = act.ScrollToPrompt(-1) },
+  { key = "t", mods = "CTRL|SHIFT", action = act.ScrollToPrompt(1) },
 
-	{ key = "l", mods = "ALT|SHIFT", action = act.ActivateCopyMode },
+  { key = "l", mods = "ALT|SHIFT", action = act.ActivateCopyMode },
 
-	-- display hints to open links with a keyboard key
-	{
-		key = "L",
-		mods = "SHIFT|CTRL",
-		action = wezterm.action.QuickSelectArgs({
-			label = "open url",
-			patterns = {
-				"https?://\\S+",
-			},
-			action = wezterm.action_callback(function(window, pane)
-				local url = window:get_selection_text_for_pane(pane)
-				wezterm.log_info("opening: " .. url)
-				wezterm.open_with(url)
-			end),
-		}),
-	},
+  -- display hints to open links with a keyboard key
+  {
+    key = "L",
+    mods = "SHIFT|CTRL",
+    action = wezterm.action.QuickSelectArgs({
+      label = "open url",
+      patterns = {
+        "https?://\\S+",
+      },
+      action = wezterm.action_callback(function(window, pane)
+        local url = window:get_selection_text_for_pane(pane)
+        wezterm.log_info("opening: " .. url)
+        wezterm.open_with(url)
+      end),
+    }),
+  },
 }
 
 config.key_tables = {
-	copy_mode = copy_mode,
+  copy_mode = copy_mode,
+}
+
+config.window_padding = {
+  left = 20,
+  right = 0,
+  top = 5,
+  bottom = 0,
 }
 
 -- and finally, return the configuration to wezterm
